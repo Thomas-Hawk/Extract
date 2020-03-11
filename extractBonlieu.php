68f7df8683b1440d074488fc6139d7444372b715
Toptom47 <thomasbozon@gmail.com>
2020-03-11 23:05:41 +0100

    in progress

--- extractBonlieu.php
+++ extractBonlieu.php
@@ -1,23 +1,25 @@
 <?php
 
-for ($u = 4536; $u < 4560; $u++) {
-    $url = "http://www.forumsirius.fr/orion/bonlieu.phtml?seance=".$u;
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $raw = curl_exec($ch);
-    curl_close($ch);
-
-    $html = new DOMDocument();
-    @$html->loadHTML($raw);
-    $xpath = new DOMXPath($html);
-    $domExemple = $xpath->query('//td[@class="scPrix"]');
-
-  
-    foreach ($domExemple as $exemple) {
-         $result = $exemple->nodeValue;
-         echo $u." ".$result."\n";
-    break;
-    }
+function extractStock()
+{
+  for ($u = 4536; $u < 4538; $u++) {
+    $url = "http://www.forumsirius.fr/orion/bonlieu.phtml?seance=" . $u;
+    $input = file_get_contents($url);
+    $content [] = $input;
+  }
+  return $content;
 }
+$list = extractStock();
+print_r ($list); 
+// $doc = new DOMDocument();
+
+// @$doc->loadHTML($input);
+// $xpath = new DOMXPath($doc);
+// $domExemple = $xpath->query('//td[@class="scPrix"]');
+
+
+// foreach ($domExemple as $exemple) {
+//   $result = $exemple->nodeValue;
+//   echo $u . " " . $result . "\n";
+//   break;
+// }
