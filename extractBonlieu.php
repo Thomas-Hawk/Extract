6002365c3af06efdc24d3c8de52629350d664433
Toptom47 <thomasbozon@gmail.com>
2020-03-22 10:53:37 +0100

    mvc

--- extractBonlieu.php
+++ extractBonlieu.php
@@ -2,24 +2,50 @@
 
 function extractStock()
 {
-  for ($u = 4536; $u < 4538; $u++) {
-    $url = "http://www.forumsirius.fr/orion/bonlieu.phtml?seance=" . $u;
-    $input = file_get_contents($url);
-    $content [] = $input;
-  }
-  return $content;
+    for ($u = 4560; $u < 4563; $u++) {
+        $url = "http://www.forumsirius.fr/orion/bonlieu.phtml?seance=" . $u;
+        $input = file_get_contents($url);
+        $content[] = $input;
+    }
+    return $content;
 }
-$list = extractStock();
-print_r ($list); 
-// $doc = new DOMDocument();
-
-// @$doc->loadHTML($input);
-// $xpath = new DOMXPath($doc);
-// $domExemple = $xpath->query('//td[@class="scPrix"]');
+// $list = extractStock();
+// print_r($list);
 
 
-// foreach ($domExemple as $exemple) {
-//   $result = $exemple->nodeValue;
-//   echo $u . " " . $result . "\n";
-//   break;
+function extractDoc($docList)
+{
+    foreach ($docList as $extract) {
+        $doc = new DOMDocument();
+        @$doc->loadHTML($extract);
+        $xpath = new DOMXPath($doc);
+        $domExemple0 = $xpath->query('//span[@class="nomSpec"]');
+        $domExemple1 = $xpath->query('//span[@class="dateSC"]');
+        $domExemple2 = $xpath->query('//td[@class="scPrix"]');
+        $dom0 = $domExemple0->item(0)->textContent;
+        $dom1 = $domExemple1->item(0)->textContent;
+        $dom2 = $domExemple2->item(0)->textContent;
+        echo $dom0 . "\n" . $dom1  . "\n" . $dom2. "\n\n";
+        // foreach ($domExemple as $exemple) {
+        //     $result = $exemple->nodeValue;
+        //     echo    $result . "\n";
+
+        // }
+    }
+}
+
+$test = extractStock();
+extractDoc($test);
+
+// echo date('d m Y ');
+
+// $result = checkdate(13, 03, 2020);
+
+// if( $result == true )
+// {
+//     echo 'la date est valide';
+// }
+// else
+// {
+//     echo 'la date n\'est pas valide';
 // }
