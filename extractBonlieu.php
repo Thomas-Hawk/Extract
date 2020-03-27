40235628e763bdc55b1c3327bf4eed3ad5d18e6e
topTomz <61994458+topTomz@users.noreply.github.com>
2020-03-27 01:14:25 +0100

    Update extractBonlieu.php
    array

--- extractBonlieu.php
+++ extractBonlieu.php
@@ -2,7 +2,7 @@
 
 function extractStock()
 {
-    for ($u = 4560; $u < 4563; $u++) {
+    for ($u = 4590; $u < 4599; $u++) {
         $url = "http://www.forumsirius.fr/orion/bonlieu.phtml?seance=" . $u;
         $input = file_get_contents($url);
         $content[] = $input;
@@ -15,23 +15,33 @@ function extractStock()
 
 function extractDoc($docList)
 {
+    $doc = new DOMDocument();
+    $elements = array('//span[@class="nomSpec"]', '//span[@class="dateSC"]', '//td[@class="scPrix"]', '//td[@class="nomActiv"]');
+
     foreach ($docList as $extract) {
-        $doc = new DOMDocument();
         @$doc->loadHTML($extract);
         $xpath = new DOMXPath($doc);
-        $domExemple0 = $xpath->query('//span[@class="nomSpec"]');
-        $domExemple1 = $xpath->query('//span[@class="dateSC"]');
-        $domExemple2 = $xpath->query('//td[@class="scPrix"]');
-        $dom0 = $domExemple0->item(0)->textContent;
-        $dom1 = $domExemple1->item(0)->textContent;
-        $dom2 = $domExemple2->item(0)->textContent;
-        echo $dom0 . "\n" . $dom1  . "\n" . $dom2. "\n\n";
-        // foreach ($domExemple as $exemple) {
-        //     $result = $exemple->nodeValue;
-        //     echo    $result . "\n";
-
-        // }
+        foreach ($elements as $element) {
+            $i = 0;
+            $domExemple0 = $xpath->query($element);
+            $dom = $domExemple0->item(0)->textContent;
+            $array[] = $dom . $i;
+            $i++;
+        }
+        // $domExemple0 = $xpath->query('//span[@class="nomSpec"]');
+        // $domExemple1 = $xpath->query('//span[@class="dateSC"]');
+        // $domExemple2 = $xpath->query('//td[@class="scPrix"]');
+        // $domExemple3 = $xpath->query('//td[@class="nomActiv"]');
+        // $dom0 = $domExemple0->item(0)->textContent;
+        // $dom1 = $domExemple1->item(0)->textContent;
+        // $dom2 = $domExemple2->item(0)->textContent;
+        // $dom3 = $domExemple3->item(0)->textContent;
+        // echo $dom0 . "\n" . $dom1  . "\n" . $dom2 . "\n\n";
+        // $array = array($dom0, $dom1, $dom2, $dom3);
     }
+    // return $array;
+    print ( $array);
+    print_r($array);
 }
 
 $test = extractStock();
